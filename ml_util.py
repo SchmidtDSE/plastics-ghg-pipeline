9cde28267841bda1ec5ba8c994d4a0a56d0d6ffe
Sam Pottinger <sam.pottinger@berkeley.edu>
2024-02-02 16:18:33 -0800

    Fix lint issues for #2.

--- ml_util.py
+++ ml_util.py
@@ -38,25 +38,25 @@ class ModelDefinition:
         self._alpha = alpha
         self._kernel = kernel
         self._degree = degree
-    
+
     def get_algorithm(self) -> str:
         return self._algorithm
-    
+
     def get_depth(self) -> OPT_INT:
         return self._depth
-    
+
     def get_estimators(self) -> OPT_INT:
         return self._estimators
-    
+
     def get_features(self) -> OPT_STR:
         return self._features
-    
+
     def get_alpha(self) -> OPT_FLOAT:
         return self._alpha
-    
+
     def get_kernel(self) -> OPT_STR:
         return self._kernel
-    
+
     def get_degree(self) -> OPT_INT:
         return self._degree
 
@@ -88,7 +88,7 @@ class ModelDefinition:
         get_int_maybe = lambda x: None if x is None else int(x)
         get_float_maybe = lambda x: None if x is None else float(x)
         get_str_maybe = lambda x: None if x is None else str(x)
-        
+
         return ModelDefinition(
             target['algorithm'],
             get_int_maybe(target.get('depth', None)),
@@ -103,14 +103,17 @@ class ModelDefinition:
 def check_linear_config(target: ModelDefinition) -> bool:
     return target.get_alpha() is not None
 
+
 def check_svr_config(target: ModelDefinition) -> bool:
     has_kernel = target.get_kernel() is not None
     has_degree = target.get_degree() is not None
     has_alpha = target.get_alpha() is not None
     return has_kernel and has_degree and has_alpha
 
+
 def check_tree_config(target: ModelDefinition) -> bool:
     return target.get_depth() is not None
+
 
 def check_random_forest_config(target: ModelDefinition) -> bool:
     has_depth = target.get_depth() is not None
@@ -118,10 +121,12 @@ def check_random_forest_config(target: ModelDefinition) -> bool:
     has_features = target.get_features() is not None
     return has_depth and has_estimators and has_features
 
+
 def check_adaboost_config(target: ModelDefinition) -> bool:
     has_depth = target.get_depth() is not None
     has_estimators = target.get_estimators() is not None
     return has_depth and has_estimators
+
 
 def build_model(target: ModelDefinition):
     assert target.is_valid()
@@ -136,14 +141,16 @@ def build_model(target: ModelDefinition):
 
     return strategy(target)
 
+
 def build_linear(target: ModelDefinition):
     return sklearn.linear_model.Ridge(alpha=target.get_alpha())
+
 
 def build_svr(target: ModelDefinition):
     model = sklearn.svm.SVR(
         kernel=target.get_kernel(),
         degree=target.get_degree(),
-        C=1-target.get_alpha()
+        C=1 - target.get_alpha()
     )
     pipeline = sklearn.pipeline.Pipeline([
         ('scale', sklearn.preprocessing.StandardScaler()),
@@ -151,8 +158,10 @@ def build_svr(target: ModelDefinition):
     ])
     return pipeline
 
+
 def build_tree(target: ModelDefinition):
     return sklearn.tree.DecisionTreeRegressor(max_depth=target.get_depth())
+
 
 def build_random_forest(target: ModelDefinition):
     return sklearn.ensemble.RandomForestRegressor(
@@ -160,6 +169,7 @@ def build_random_forest(target: ModelDefinition):
         max_depth=target.get_depth(),
         max_features=target.get_features()
     )
+
 
 def build_adaboost(target: ModelDefinition):
     base_estimator = sklearn.tree.DecisionTreeRegressor(max_depth=target.get_depth())
