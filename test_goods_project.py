6dc05c6d4887de32ad13da3cb55105f369471a41
A Samuel Pottinger <sam@gleap.org>
2024-02-05 01:10:10 +0000

    Add tests for goods project.

---
+++ test_goods_project.py
@@ -0,0 +1,22 @@
+import unittest
+
+import data_struct
+import goods_project
+
+
+class GoodsProjectTests(unittest.TestCase):
+
+    def setUp(self):
+        self._index = data_struct.KeyingObservationIndex()
+        self._index.add(2023, 'NAFTA', 'Transportation', data_struct.Observation(1, 2, 3))
+        self._task = goods_project.GoodsProjectionTask()
+
+    def test_get_observation_dict_none(self):
+        serialized = self._task._get_observation_dict(self._index, 2024, 'NAFTA', 'Transportation')
+        self.assertIsNone(serialized)
+
+    def test_get_observation_dict_success(self):
+        serialized = self._task._get_observation_dict(self._index, 2023, 'NAFTA', 'Transportation')
+        self.assertIsNotNone(serialized)
+        self.assertEqual(serialized['year'], 2023)
+        self.assertEqual(serialized['ratioSector'], 1)
